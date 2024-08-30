5955abf98cf908ae6561919dfdc10b4a7f642287
Matteus Santos S <matteus.py@gmail.com>
2024-08-30 19:35:08 -0300

    feat: add create investment service

---
+++ src/services/investment/create-investment.service.ts
@@ -0,0 +1,30 @@
+import { randomUUID } from 'node:crypto'
+import type { Investment } from '../../entities/investment'
+import type { InvestmentRepository } from '../../repositories/investment.repository'
+
+export interface createInvestmentRequest {
+  title: string
+  ownerUUID: string
+  initialAmount: number
+  createdAt?: Date
+}
+
+export class CreateInvestmentService {
+  constructor(private readonly investmentRepository: InvestmentRepository) {}
+  async execute(request: createInvestmentRequest): Promise<Investment | Error> {
+    if (request.initialAmount <= 0) new Error('initial amount cannot be negative')
+    if (request.title.length <= 2 || request.title.length > 255) {
+      return new Error('title must be less than 2 and less than 255')
+    }
+
+    const investment: Investment = {
+      ...request,
+      uuid: randomUUID(),
+      status: 'ACTIVE',
+      createdAt: request.createdAt ?? new Date(),
+    }
+
+    await this.investmentRepository.save(investment)
+    return investment
+  }
+}
