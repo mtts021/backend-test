9d068e9bfb1a236f495ea2db7b23f7c67dce07b8
Matteus Santos S <matteus.py@gmail.com>
2024-09-04 14:55:59 -0300

    feat: add get investment service

---
+++ src/services/investment/get-investment.service.ts
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs'
+import type { InvestmentRepository } from '../../repositories/investment.repository'
+
+export class GetInvestmentService {
+  constructor(private readonly investmentRepository: InvestmentRepository) {}
+  async execute(investmentUUID: string) {
+    const investment =
+      await this.investmentRepository.findInvestmentByUUID(investmentUUID)
+    if (!investment) {
+      return new Error('Investment not found')
+    }
+
+    const createdAtFromDayjs = dayjs(investment.createdAt)
+    const differenceMonth = Math.abs(createdAtFromDayjs.diff(new Date(), 'month'))
+    if (investment.status === 'ACTIVE') {
+      const expectedBalance = investment.initialAmount * (1 + 0.0052) ** differenceMonth
+
+      return {
+        investment,
+        expectedBalance: expectedBalance.toFixed(4),
+      }
+    }
+    const balanceGain =
+      investment.initialAmount * (1 + 0.0052) ** differenceMonth -
+      investment.initialAmount
+
+    return {
+      investment,
+      balanceGain,
+    }
+  }
+}
