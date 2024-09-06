e2490f8c1212f85c72178af6eabd359a0c44d68f
Matteus Santos S <matteus.py@gmail.com>
2024-09-06 10:12:30 -0300

    refactor: separate calculation expected balance of get investment service

--- src/services/investment/get-investment.service.ts
+++ src/services/investment/get-investment.service.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 import type { InvestmentRepository } from '../../repositories/investment.repository'
+import { calculateExpectedBalance } from './helpers/calculate-gain.helper'
 
 export class GetInvestmentService {
   constructor(private readonly investmentRepository: InvestmentRepository) {}
@@ -13,7 +14,10 @@ export class GetInvestmentService {
     const createdAtFromDayjs = dayjs(investment.createdAt)
     const differenceMonth = Math.abs(createdAtFromDayjs.diff(new Date(), 'month'))
     if (investment.status === 'ACTIVE') {
-      const expectedBalance = investment.initialAmount * (1 + 0.0052) ** differenceMonth
+      const expectedBalance = calculateExpectedBalance(
+        investment.initialAmount,
+        differenceMonth,
+      )
 
       return {
         investment,
