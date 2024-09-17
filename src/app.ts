70410957f463180c005a096235207f0d778983a5
Matteus Santos S <matteus.py@gmail.com>
2024-09-17 11:12:32 -0300

    feat: add error handler in fastify

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-import fastify from 'fastify'
+import fastify, {
+  type FastifyError,
+  type FastifyRequest,
+  type FastifyReply,
+} from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import { ZodError } from 'zod'
 import { ownerRoute } from './external/http/routes/owner.route'
 
 export class App {
@@ -22,5 +27,19 @@ export class App {
   plugins() {
     this.server.setValidatorCompiler(validatorCompiler)
     this.server.setSerializerCompiler(serializerCompiler)
+    this.server.setErrorHandler(
+      (error: FastifyError, req: FastifyRequest, res: FastifyReply) => {
+        if (error instanceof ZodError) {
+          res.status(400).send({
+            error: {
+              code: 'INVALID_INPUT',
+              details: error.flatten().fieldErrors,
+            },
+          })
+        }
+
+        res.status(500).send({ message: 'Internal server error' })
+      },
+    )
   }
 }
