319e5456d429e6082ee650e889e8087f6509f6e4
Matteus Santos S <matteus.py@gmail.com>
2024-09-18 16:49:40 -0300

    test: corrected the create investmet service test

--- src/services/investment/create-investment.service.spec.ts
+++ src/services/investment/create-investment.service.spec.ts
@@ -1,15 +1,27 @@
 import { describe, expect, it, vi } from 'vitest'
 import { InMemoryInvestmentRepository } from '../../../tests/repositories/in-memory-investment.repository'
+import { InMemoryOwnerRepository } from '../../../tests/repositories/in-memory-owner.repository'
 import { CreateInvestmentService } from './create-investment.service'
 
 describe('Create investment Service', () => {
+  const ownerRepository = new InMemoryOwnerRepository()
+  ownerRepository.owners.push({
+    uuid: '16d91145-978d-4534-8f0f-178431ffd997',
+    name: 'john',
+    email: 'john@email.com',
+    password: 'pw12345',
+    createdAt: new Date(),
+  })
   it('should be able create investment', async () => {
     const investmentRepository = new InMemoryInvestmentRepository()
-    const createInvestmentService = new CreateInvestmentService(investmentRepository)
+    const createInvestmentService = new CreateInvestmentService(
+      investmentRepository,
+      ownerRepository,
+    )
 
     const investment = await createInvestmentService.execute({
       title: 'Title fake',
-      ownerUUID: 'c8c3h8hc33hc893hv3rv4tv89',
+      ownerUUID: '16d91145-978d-4534-8f0f-178431ffd997',
       initialAmount: 100,
     })
 
@@ -19,24 +31,30 @@ describe('Create investment Service', () => {
 
   it('should be able return an error if initial amount is negative or 0', async () => {
     const investmentRepository = new InMemoryInvestmentRepository()
-    const createInvestmentService = new CreateInvestmentService(investmentRepository)
+    const createInvestmentService = new CreateInvestmentService(
+      investmentRepository,
+      ownerRepository,
+    )
 
     expect(
       await createInvestmentService.execute({
         title: 'title fake',
-        ownerUUID: 'c8c3h8hc33hc893hv3rv4tv89',
+        ownerUUID: '16d91145-978d-4534-8f0f-178431ffd997',
         initialAmount: 0,
       }),
     ).toBeInstanceOf(Error)
   })
   it('should be able return error if title less than 2', async () => {
     const investmentRepository = new InMemoryInvestmentRepository()
-    const createInvestmentService = new CreateInvestmentService(investmentRepository)
+    const createInvestmentService = new CreateInvestmentService(
+      investmentRepository,
+      ownerRepository,
+    )
 
     expect(
       await createInvestmentService.execute({
         title: '',
-        ownerUUID: 'c8c3h8hc33hc893hv3rv4tv89',
+        ownerUUID: '16d91145-978d-4534-8f0f-178431ffd997',
         initialAmount: 100,
       }),
     ).toBeInstanceOf(Error)
@@ -44,7 +62,11 @@ describe('Create investment Service', () => {
 
   it('should be able return error if title less than 2', async () => {
     const investmentRepository = new InMemoryInvestmentRepository()
-    const createInvestmentService = new CreateInvestmentService(investmentRepository)
+    const ownerRepository = new InMemoryOwnerRepository()
+    const createInvestmentService = new CreateInvestmentService(
+      investmentRepository,
+      ownerRepository,
+    )
 
     expect(
       await createInvestmentService.execute({
