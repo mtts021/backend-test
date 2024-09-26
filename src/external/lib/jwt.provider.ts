f5e1e55922e46059ab790f99baff585d3102a39a
Matteus Santos S <matteus.py@gmail.com>
2024-09-25 23:16:15 -0300

    feat(JwtTokenProvider): implements verify token

--- src/external/lib/jwt.provider.ts
+++ src/external/lib/jwt.provider.ts
@@ -1,8 +1,20 @@
-import jwt, { type SignOptions } from 'jsonwebtoken'
+import jwt, { type SignOptions, JwtPayload } from 'jsonwebtoken'
 import type { TokenProvider, payloadData } from '../../services/ports/token.provider'
 import 'dotenv/config'
 
 export class JwtTokenProvider implements TokenProvider {
+  async verifyToken(token: string): Promise<payloadData | Error> {
+    try {
+      const result = jwt.verify(token, <string>process.env.SECRET_KEY_JWT)
+      if (typeof result !== 'object' || !result.uuid) {
+        return new Error('invalid token')
+      }
+      const uuid = result.uuid
+      return { uuid }
+    } catch (error) {
+      return new Error('invalid token')
+    }
+  }
   async createToken(payload: payloadData): Promise<string> {
     const { uuid } = payload
     const options: SignOptions = { expiresIn: '7d' }
